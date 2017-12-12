215fd8f777900fa07e3278b64be6861597fefcce
Kumar Reddy <kumarreddy72@gmail.com>
2017-12-12 15:13:02 +0530

    committing initial project

---
+++ WKWebView-JS-Examples/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  WKWebView-JS-Examples
+//
+//  Created by Birapuram Kumar Reddy on 12/12/17.
+//  Copyright © 2017 KRiOSApps. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    var wkWebView:WKWebView!
+    let versionHandler = "VersionHandler"
+    let verifyHandler = "VerifyHandler"
+
+    override func loadView() {
+        super.loadView()
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController.add(self, name: versionHandler)
+        configuration.userContentController.add(self, name: verifyHandler)
+        wkWebView = WKWebView(frame: view.frame, configuration: configuration)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        loadHtmlIntoWebview()
+    }
+
+    internal func loadHtmlIntoWebview() -> Void {
+        let fileURL = Bundle.main.url(forResource: "sample", withExtension: "html")
+        wkWebView.loadFileURL(fileURL!, allowingReadAccessTo: fileURL!)
+    }
+
+}
+
+extension ViewController : WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        switch message.name {
+        case versionHandler:
+            let mobileVersion = "1.0.0" // you can write custom logic here.
+            let script = "receivedMobileVersion('\(mobileVersion)')"
+            self.wkWebView.evaluateJavaScript(script, completionHandler: { (data, error) in
+                if let err = error{
+                    print(err)
+                }
+            })
+        case verifyHandler:
+            print("Validation of version")
+            print(message.body)
+        default:
+            print(message.body)
+        }
+    }
+}
+
+
+
